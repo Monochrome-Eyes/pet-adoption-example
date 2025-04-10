0fa986fb88722bb67d1ea27075d8b3e9a5fd0434
Samuel Shivy <sshivy7@gmail.com>
2025-04-09 20:01:33 -0400

    Created the EFPetRepository skeleton

---
+++ Repositories/EFPetRepository.cs
@@ -0,0 +1,25 @@
+using midterm_project.Models;
+
+namespace midterm_project.Repositories;
+
+public class EFPetRepository: IPetRepository {
+	public Pet CreatePet(Pet pet) {
+		throw new NotImplementedException();
+	}
+
+	public Pet GetPetById(int id) {
+		throw new NotImplementedException();
+	}
+
+	public IEnumerable<Pet> GetAllPets() {
+		throw new NotImplementedException();
+	}
+
+	public Pet UpdatePet(Pet pet) {
+		throw new NotImplementedException();
+	}
+
+	public void DeletePetById(int id) {
+		throw new NotImplementedException();
+	}
+}
